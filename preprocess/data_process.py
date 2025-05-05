d4f16732a290306c27fb1c1d4e2fd773c3662812
Gurpranked <gurpreet_singh@student.uml.edu>
2025-05-05 10:59:05 -0400

    progress: Audited data split strategy: Split first, the augment. Train: 6K, Val: 200, Test: 500

--- preprocess/data_process.py
+++ preprocess/data_process.py
@@ -27,35 +27,43 @@ def create_dir(path):
     if not os.path.exists(ROOT_DATA_PATH + path):
         os.makedirs(ROOT_DATA_PATH + path)
 
-def augment_image(image: torch.tensor):
+def augment_image(image: torch.tensor, index: int):
     """
-    Randomly apply one or more transformations to the image
+    Apply one of the 4 augmentation methods to an image, based on a provided index
     """
-    
-    transform_pipeline = v2.Compose([
-        v2.RandomRotation(random.uniform(0, 360)),
-        v2.RandomHorizontalFlip(p=1.0),
-        v2.RandomVerticalFlip(p=1.0),
-        v2.GaussianBlur(kernel_size=(3, 3))
-    ])
+    transforms = [
+        v2.RandomRotation(random.uniform(0, 360)), 
+        v2.RandomHorizontalFlip(p=1.0), 
+        v2.RandomVerticalFlip(p=1.0), 
+        v2.GaussianBlur(kernel_size=(3,3)),
+        v2.ColorJitter()
+    ]
 
-    return transform_pipeline(image)
+    return transforms[index%5](image)
 
 def pad_images_with_augmentations(original_images: list[torch.tensor], desired_size: int):
-    
     """
     Supplement the image label pairs with augmentations to match the desired quantity
     """
     
-    num_augmentations = max(1, (desired_size - len(original_images)) // len(original_images))
+    if len(original_images) >= desired_size:
+        print("No padding needed. Desired size is already met.")
+        return original_images
 
-    padded_images = []
-    
-    for i in range(num_augmentations):
-        for image in original_images:
-            padded_images.append(augment_image(image))
+    num_augmentations = desired_size - len(original_images)
+    print(f"Num Augmentations to compute: {num_augmentations}")
+    augmented_images = []
+    num_images = len(original_images)
 
-    return padded_images
+    while num_augmentations > 0:
+        num_augmentations -= 1
+        # Can use num_augmentations as index since it's being changed at each iteration anyways
+        augmented_images.append(
+            augment_image(
+                original_images[num_augmentations%num_images], 
+                num_augmentations))
+
+    return original_images + augmented_images
 
 def load_image(path):
     """
@@ -63,7 +71,7 @@ def load_image(path):
     Normalizes and converts to Tensor [0, 255]
     """
     transform = v2.Compose([
-        v2.PILToTensor()
+        v2.ToTensor()
     ])
     return transform(Image.open(path))
 
@@ -72,7 +80,7 @@ class ShipsDataset(Dataset):
     """
     A dataset for the ships images
     """
-    def __init__(self, image_label_pairs: list[tuple[torch.tensor, int]], transform=None):
+    def __init__(self, image_label_pairs: list[tuple[torch.tensor, float]], transform=None):
         super().__init__()
         self.image_label_pairs = image_label_pairs
         self.transform = transform
@@ -80,7 +88,7 @@ class ShipsDataset(Dataset):
     def __len__(self):
         return len(self.image_label_pairs)
     
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> tuple[torch.tensor, float]:
         sample = self.image_label_pairs[index][0]
 
         if self.transform:
@@ -89,7 +97,7 @@ class ShipsDataset(Dataset):
         return (sample, self.image_label_pairs[index][1])
 
 
-def split_set(data_list: list, split_ratio: float, shuffle=False):
+def split_set(data_list: list, split_ratio_train: float, split_ratio_val: float, shuffle=False) -> tuple[list, list, list]:
     """
     Splits the data list into two parts based on a split ratio
     Also optionally shuffles the data list before splitting
@@ -97,63 +105,94 @@ def split_set(data_list: list, split_ratio: float, shuffle=False):
     if shuffle:
        random.shuffle(data_list)
     
-    split_index = int(len(data_list) * split_ratio)
+    split_index_train = int(len(data_list) * split_ratio_train)
+    split_index_val = int(len(data_list) * (split_ratio_val + split_ratio_train))
 
-    return data_list[:split_index], data_list[split_index:]
+    # Train, Validation, Test
+    return data_list[:split_index_train], data_list[split_index_train:split_index_val], data_list[split_index_val:]
 
 
 
-def get_dataloaders(ships_samples_target_amount=4000, nonships_samples_target_amount=4000, train_split=0.8, val_split=0.1, random_seed = 42) -> tuple[DataLoader, DataLoader, DataLoader]:
+def get_dataloaders(random_seed = 42) -> tuple[DataLoader, DataLoader, DataLoader]:
     """
     Creates the dataloaders for train, validation and test sets 
     """
-    # create_dir("formatted/train/ships")
-    # create_dir("formatted/train/nonships")
-    # create_dir("formatted/val/ships")
-    # create_dir("formatted/val/nonships")
-    # create_dir("formatted/test/ships")
-    # create_dir("formatted/test/nonships")
-    random.seed(random_seed)
 
-    images_paths = os.listdir(IMAGES_PATH)
-    # labels = [int(path.split("__")[0] for path in images_paths)]
-    
-    ships_paths = []
-    nonships_paths = []
+    if os.path.exists('train_set.pt'):
+        train_set = torch.load('train_set.pt', weights_only=False)
+        print(f"Train Set Size: {len(train_set)}")
+        train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+    if os.path.exists('val_set.pt'):
+        val_set = torch.load('val_set.pt', weights_only=False)
+        print(f"Val Set Size: {len(val_set)}")
+        val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
+    if os.path.exists('test_set.pt'):
+        test_set = torch.load('test_set.pt', weights_only=False)
+        print(f"Test Set Size: {len(test_set)}")
+        test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
+    else:    
+        random.seed(random_seed)
 
-    for path in images_paths:
-        category = int(path.split("__")[0])
-        if category == 0:
-            nonships_paths.append(load_image(IMAGES_PATH + path))
-        else:
-            ships_paths.append(load_image(IMAGES_PATH + path))
-
-    padded_ships = pad_images_with_augmentations(ships_paths, ships_samples_target_amount)
-    padded_nonships = pad_images_with_augmentations(nonships_paths, nonships_samples_target_amount)
-
-    padded_ships_with_labels = [(image, 1) for image in padded_ships]
-    padded_nonships_with_labels = [(image, 0) for image in padded_nonships]
-
-    # Combine the images of both classes
-    images_with_labels = padded_ships_with_labels + padded_nonships_with_labels
-
-    # Split the dataset into train, validation, and test sets
-    train_set, test_set = split_set(images_with_labels, train_split, shuffle=True)
-    train_set, val_set = split_set(train_set, val_split, shuffle=False)
-
-    # Create Dataset
-    train_set = ShipsDataset(train_set, transform=None)
-    val_set = ShipsDataset(val_set, transform=None)
-    test_set = ShipsDataset(test_set, transform=None)
-    torch.save(train_set, 'train_set.pt')
-    torch.save(val_set, 'val_set.pt')
-    torch.save(test_set, 'test_set.pt')
-    
-    # Create the data loaders
-    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
-    test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
+        images_paths = os.listdir(IMAGES_PATH)
+        # labels = [int(path.split("__")[0] for path in images_paths)]
         
+        ships = []
+        nonships = []
+
+        for path in images_paths:
+            category = int(path.split("__")[0])
+            if category == 0:
+                nonships.append(load_image(IMAGES_PATH + path))
+            else:
+                ships.append(load_image(IMAGES_PATH + path))
+
+        test_size = 250
+        val_size = 100
+
+        # Randomly sample 250 images from each category for test set
+        test_ships = ships[:test_size]
+        test_nonships = nonships[:test_size]
+
+        # Randomly sample 100 images from each category for val set
+        val_ships = ships[test_size:test_size + val_size]
+        val_nonships = nonships[test_size:test_size + val_size]
+
+        # Pad the remaining images to 3000 for each class
+        ships_padded = pad_images_with_augmentations(ships[test_size + val_size:], 3000)
+        print(f"Ships padded images: {len(ships_padded)}")
+        nonships_padded = pad_images_with_augmentations(nonships[test_size + val_size:], 3000)
+        print(f"Ships non-padded images: {len(nonships_padded)}")
+
+        # Add corresponding labels to all data
+        test_ships_with_labels = [(image, 1) for image in test_ships]
+        test_nonships_with_labels = [(image, 0) for image in test_nonships]
+        val_ships_with_labels = [(image, 1) for image in val_ships]
+        val_nonships_with_labels = [(image, 0) for image in val_nonships]
+        train_ships_with_labels = [(image, 1) for image in ships_padded]
+        train_nonships_with_labels = [(image, 0) for image in nonships_padded]
+
+        # Combine and shuffle data for each set
+        test_set = test_ships_with_labels + test_nonships_with_labels
+        val_set = val_ships_with_labels + val_nonships_with_labels
+        train_set = train_ships_with_labels + train_nonships_with_labels
+        print(f"Train Set Size: {len(train_set)}")
+        print(f"Test Set Size: {len(test_set)}")
+        print(f"Val Set Size: {len(val_set)}")
+
+        # Create Dataset
+        train_set = ShipsDataset(train_set, transform=None)
+        val_set = ShipsDataset(val_set, transform=None)
+        test_set = ShipsDataset(test_set, transform=None)
+        torch.save(train_set, 'train_set.pt')
+        torch.save(val_set, 'val_set.pt')
+        torch.save(test_set, 'test_set.pt')
+        
+        # Create the data loaders
+        train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+        val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
+        test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True)
+
+    # Return the data loaders       
     return train_loader, val_loader, test_loader
 
 
