cbb8052f0f1e771d422499168095c3253f2b4666
Gurpranked <gurpreet_singh@student.uml.edu>
2025-05-07 14:11:57 -0400

    progress: Updated dataset downloading script with more details

--- download_dataset.py
+++ download_dataset.py
@@ -1,8 +1,9 @@
 import os, stat
+import argsparse
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 # Requires Kaggle account with configured credentials via API key
-def download_dataset(destination="/home/public/datasets/ships-in-satellite/"):
+def download_dataset(destination: str):
     # Initialize and authenticate the Kaggle API
     api = KaggleApi()
     api.authenticate()
@@ -22,4 +23,9 @@ def download_dataset(destination="/home/public/datasets/ships-in-satellite/"):
     print(f"Dataset successfully downloaded to {destination}")
 
 if __name__ == "__main__":
-    download_dataset()
+    parser = argsparse.ArgumentParser()
+    parser.add_argument('--dest', type=str, description='Where to download dataset', required=True)
+    args = parser.parse_args()
+    print("Downloading Dataset to " + args.dest + ". NOTE: This may take a while. Pleaset")
+    download_dataset(args.dest)
+    print("Dataset Downloaded to " + args.dest + f". NOTE: Only the owner {os.getuid()} has access to the directory.")
